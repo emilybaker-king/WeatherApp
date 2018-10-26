a0268f3da8d6e2705e113e814df35fa1f9d28808
Emily Baker-King <ebakerking@gmail.com>
2018-10-26 13:01:41 -0500

    added default UI

--- WeatherApp/WeatherDisplayViewController.swift
+++ WeatherApp/WeatherDisplayViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherDisplayViewController.swift
 //  WeatherApp
 //
 //  Created by Emily Baker-King on 10/24/18.
@@ -11,11 +11,21 @@ import Alamofire
 import SwiftyJSON
 
 
-class ViewController: UIViewController {
+class WeatherDisplayViewController: UIViewController {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var iconLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var highTempLabel: UILabel!
+    @IBOutlet weak var lowTempLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpDefaultUI()
         
         let apiKeys = APIKeys()
         
@@ -28,7 +38,9 @@ class ViewController: UIViewController {
         
         let request = Alamofire.request(url)
         
+        //carry out the request here
         request.responseJSON { response in
+            //switch based on result of the request
             switch response.result {
             case .success(let value):
                 //if our request succeeds, take the value and convert it into a JSON object
@@ -46,15 +58,28 @@ class ViewController: UIViewController {
         
         let googleRequest = Alamofire.request(googleRequestURL)
         
+        //carry out request here
         googleRequest.responseJSON { response in
+            //switching based on result of the request
             switch response.result {
             case .success(let value):
+                //if our request succeeds, take the value and convert it into a JSON object
                 let json = JSON(value)
+                //print out the JSON object
                 print(json)
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    //this function will give the UI some default whenever we first load the app
+    func setUpDefaultUI() {
+        locationLabel.text = ""
+        iconLabel.text = "☀️"
+        currentTempLabel.text = "Enter a location please!"
+        highTempLabel.text = "-"
+        lowTempLabel.text = "-"
     }
 
 
